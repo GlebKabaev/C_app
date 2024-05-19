52043c59b551675e6ba44fe8b930ffb401245d98
GlebKabaev <112932953+GlebKabaev@users.noreply.github.com>
2024-05-19 08:34:51 +0300

    Add files via upload

--- CappZ/rabota2/rabota2/ReportForm.Designer.cs
+++ CappZ/rabota2/rabota2/ReportForm.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ReportForm";
+            accept = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            dataGridView1 = new DataGridView();
+            richTextBox1 = new RichTextBox();
+            button_update = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // accept
+            // 
+            accept.Location = new Point(456, 173);
+            accept.Name = "accept";
+            accept.Size = new Size(142, 23);
+            accept.TabIndex = 0;
+            accept.Text = "сформировать";
+            accept.UseVisualStyleBackColor = true;
+            accept.Click += accept_Click;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(83, 110);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(471, 110);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(200, 23);
+            dateTimePicker2.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(131, 202);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(505, 216);
+            dataGridView1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(266, 26);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(219, 44);
+            richTextBox1.TabIndex = 4;
+            richTextBox1.Text = "";
+            // 
+            // button_update
+            // 
+            button_update.Location = new Point(161, 173);
+            button_update.Name = "button_update";
+            button_update.Size = new Size(137, 23);
+            button_update.TabIndex = 5;
+            button_update.Text = "обновить";
+            button_update.UseVisualStyleBackColor = true;
+            button_update.Click += button_update_Click;
+            // 
+            // ReportForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button_update);
+            Controls.Add(richTextBox1);
+            Controls.Add(dataGridView1);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(accept);
+            Name = "ReportForm";
+            Text = "ReportForm";
+            Load += ReportForm_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button accept;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private DataGridView dataGridView1;
+        private RichTextBox richTextBox1;
+        private Button button_update;
     }
 }
--- CappZ/rabota2/rabota2/ReportForm.cs
+++ CappZ/rabota2/rabota2/ReportForm.cs
@@ -8,18 +8,151 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Npgsql;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.VisualBasic.ApplicationServices;
+
 
 namespace rabota2
 {
     public partial class ReportForm : Form
     {
         public NpgsqlConnection con;
-        DataTable dt = new DataTable();
+        System.Data.DataTable dt = new System.Data.DataTable();
         DataSet ds = new DataSet();
         public ReportForm(NpgsqlConnection con)
         {
+
             InitializeComponent();
             this.con = con;
+
+        }
+
+        private void accept_Click(object sender, EventArgs e)
+        {
+            ExportToExcel(dataGridView1);
+        }
+        private void ExportToExcel(DataGridView dataGridView)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Workbooks.Add();
+            Excel._Worksheet worksheet = (Excel._Worksheet)excelApp.ActiveSheet;
+            excelApp.Visible = true;
+
+            // Заголовки столбцов
+            for (int i = 0; i < dataGridView.Columns.Count; i++)
+            {
+                worksheet.Cells[1, i + 1] = dataGridView.Columns[i].HeaderText;
+            }
+
+            // Данные строк
+            for (int i = 0; i < dataGridView.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataGridView.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView.Rows[i].Cells[j].Value;
+                }
+
+            }
+
+
+            // Опционально: автоматическое форматирование столбцов
+            worksheet.Columns.AutoFit();
+
+            // Сохранение файла (опционально)
+            // string filePath = @"C:\path\to\your\file.xlsx";
+            // worksheet.SaveAs(filePath);
+
+            // Освобождение ресурсов
+            releaseObject(worksheet);
+            releaseObject(excelApp);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+
+        private void ReportForm_Load(object sender, EventArgs e)
+        {
+           /* int day1 = dateTimePicker1.Value.Day;
+            int month1 = dateTimePicker1.Value.Month;
+            int year1 = dateTimePicker1.Value.Year;
+            int day2 = dateTimePicker2.Value.Day;
+            int month2 = dateTimePicker2.Value.Month;
+            int year2 = dateTimePicker2.Value.Year;
+            String sql = String.Format("select * from futura where data>= '{1}-{2}-{3}' and data<= '{4}-{5}-{6}'", day1, month1, year1, day2, month2, year2);
+            richTextBox1.AppendText(sql);
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, con);
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns[0].HeaderText = "Номер";
+            dataGridView1.Columns[1].HeaderText = "Номер поставщика";
+            dataGridView1.Columns[2].HeaderText = "Дата";
+            dataGridView1.Columns[3].HeaderText = "Сумма";
+            StartPosition = FormStartPosition.CenterScreen;*/
+        }
+
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            int day1 = dateTimePicker1.Value.Day;
+            int month1 = dateTimePicker1.Value.Month;
+            int year1 = dateTimePicker1.Value.Year;
+            int day2 = dateTimePicker2.Value.Day;
+            int month2 = dateTimePicker2.Value.Month;
+            int year2 = dateTimePicker2.Value.Year;
+            String sql = String.Format("select * from futura where data>= '{0}-{1}-{2}' and data<= '{3}-{4}-{5}'", year1, month1, day1, year2, month2, day2);
+            richTextBox1.AppendText(sql);
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, con);
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns[0].HeaderText = "Номер";
+            dataGridView1.Columns[1].HeaderText = "Номер поставщика";
+            dataGridView1.Columns[2].HeaderText = "Дата";
+            dataGridView1.Columns[3].HeaderText = "Сумма";
+            StartPosition = FormStartPosition.CenterScreen;
         }
     }
 }
+
+
+
+
+
+
+
+/*public void Update()
+        {
+            
+            String sql = "Select * from futura";
+            richTextBox1.AppendText(sql);
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, con);
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            dataGridView1.DataSource = dt;
+            dataGridView1.Columns[0].HeaderText = "Номер";
+            dataGridView1.Columns[1].HeaderText = "Номер поставщика";
+            dataGridView1.Columns[2].HeaderText = "Дата";
+            dataGridView1.Columns[3].HeaderText = "Сумма";
+            StartPosition = FormStartPosition.CenterScreen;
+
+        }*/
+
